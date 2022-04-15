67ed5e2ef3c0e81fb73207213cee162368aa04b0
Juri Grabowski <debian@jugra.de>
2022-04-15 23:42:12 +0200

    formatting

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
 DOWNLOADS="\
-https://fai-project.org/netboot/amd64/REPLACE_VERSION/vmlinuz|vmlinuz \
-https://fai-project.org/netboot/amd64/REPLACE_VERSION/initrd.img|initrd.img \
+https://fai-project.org/netboot/amd64/REPLACE_VERSION/vmlinuz|vmlinuz
+https://fai-project.org/netboot/amd64/REPLACE_VERSION/initrd.img|initrd.img
 https://fai-project.org/netboot/amd64/REPLACE_VERSION/filesystem.squashfs|filesystem.squashfs"
